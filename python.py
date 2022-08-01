2710a46647a5ea95d60b6b46750145157b4c631f
Lokesh-R07 <96984987+Lokesh-R07@users.noreply.github.com>
2022-08-01 15:20:43 +0530

    Updated code

--- python.py
+++ python.py
@@ -1,1 +1,5 @@
-dsfgvkUA FBTEAUVGEAKTVETBIUEL, 
+maximum = int(input(" Please Enter the Maximum Value : "))
+
+for number in range(1, maximum+1):
+    if(number % 2 == 0):
+        print("{0}".format(number))
